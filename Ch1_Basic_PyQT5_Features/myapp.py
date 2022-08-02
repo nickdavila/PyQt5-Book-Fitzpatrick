5b022eeddd5a06ab988c787267fd7ac6ba8060e8
nickdavila <ndavila@utexas.edu>
2022-08-02 02:37:29 -0500

    Bookmark at Page 14 of book
    Finished reading page 13 which explains what every line of code does. Now I'm just starting page 14  but deciding to go to bed and rest.

--- Ch1_Basic_PyQT5_Features/myapp.py
+++ Ch1_Basic_PyQT5_Features/myapp.py
@@ -6,3 +6,18 @@ from PyQt5.QtWidgets import QApplication, QWidget
 #Only needed for access to command line arguments
 import sys
 
+
+# You need one (and only one) QApplication instance per application.
+# Pass in sys.argv to allow command line arguments for your app.
+# If you know you won't use command line arguments QApplications([]) works too.
+app = QApplication(sys.argv)
+
+# Create a Qt widget, which will be our window.
+window = QWidget()
+window.show()   # IMPORTANT!!! Windows are hidden by default. .show() makes it visible
+
+# Start the event loop.
+app.exec_()
+
+
+# Application won't reach here until you exit and the event loop has stopped.
